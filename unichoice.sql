b875b39a86d2da83b59c9059d48092d9f17d020a
kaitlynliang <51792838+kaitlynliang@users.noreply.github.com>
2020-06-28 04:07:52 -0400

    Update unichoice.sql

--- unichoice.sql
+++ unichoice.sql
@@ -4,7 +4,5 @@ Search through and pick out people w certain uni,
 Output person name, university, URL, faculty URL, numpubs, Research areas
 */
 SELECT People.LF_Name, People.URL, People.NumbPublications, People.FacultyURL, ResearchArea.Name FROM people 
-INNER JOIN ResearchArea.Name ON ResearchArea.ID=People.RA_ID
-WHERE UniversityID IN 
-    (SELECT ID FROM University 
-    WHERE Name = 'b') 
+INNER JOIN ResearchArea.Name ON ResearchArea.ID=People.RA_ID WHERE UniversityID IN 
+    (SELECT ID FROM University     WHERE Name = 'b') 
